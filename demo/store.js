028518167fbae829fc9b703bd9369cda0ba3411c
Alexander Reid <alexander.a.reid@espn.com>
2018-07-31 14:24:58 -0400

    Adding Demos 02

--- demo/store.js
+++ demo/store.js
@@ -1,15 +1,31 @@
-import uniops from '../index.js'
-import unistore from 'unistore'
-import devtools    from 'unistore/devtools'
+import uniops         from '../index.js'
+import unistore       from 'unistore'
+import devtools       from 'unistore/devtools'
+import request        from 'request'
+import babelpolyfill  from 'babel-polyfill'
 
-const genBigArray = (length, max) => [...new Array(length)].map(() => Math.round(Math.random() * max));
-const initialState = {
- bigArray: genBigArray(10000, 100),
- bigObj: {},
- bigStr: "",
- bigCnv: ""
-}
 
-const store = devtools(unistore(initialState));
+(async () => {
 
-console.log(store);
+  const genBigArray = (length, max) => [...new Array(length)]
+        .map(() => Math.round(Math.random() * max));
+
+  const genBigObj = await new Promise(resolve => {
+      var xhr = new XMLHttpRequest();
+      xhr.open("GET", 'https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=USD&limit=10000', true);
+      xhr.onload = (e) => resolve(JSON.parse(xhr.response));
+      xhr.onerror = () => resolve(undefined);
+      xhr.send();
+   });
+
+  const initialState = {
+   bigArray: genBigArray(10000, 100),
+   bigObj: genBigObj.Data,
+   bigStr: JSON.stringify(genBigObj.Data),
+   bigCnv: ""
+  }
+  const store = devtools(unistore(initialState));
+
+})().catch(err => {
+    console.error(err);
+});
