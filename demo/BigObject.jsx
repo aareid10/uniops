08d37fb4a9c5cf897b2cc3d281f79d91324d1cfa
Alexander Reid <alexander.a.reid@espn.com>
2018-08-01 10:45:14 -0400

    Demo: Refactoring Store

--- demo/BigObject.jsx
+++ demo/BigObject.jsx
@@ -1,5 +1,15 @@
 import { h, Component } from 'preact';
 
+(async () => {
+  const genBigObj = await new Promise(resolve => {
+      var xhr = new XMLHttpRequest();
+      xhr.open("GET", 'https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=USD&limit=10000', true);
+      xhr.onload = (e) => resolve(JSON.parse(xhr.response));
+      xhr.onerror = () => resolve(undefined);
+      xhr.send();
+   });
+})();
+
 export default class BigObject extends Component {
   render() {
     return (
