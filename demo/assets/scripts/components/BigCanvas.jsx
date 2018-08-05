b6e15e3931f4d26f08176afdfb5782e3d45a00cf
Alexander Reid <alexander.a.reid@espn.com>
2018-08-05 00:57:19 -0400

    Canvas examples 12

--- demo/assets/scripts/components/BigCanvas.jsx
+++ demo/assets/scripts/components/BigCanvas.jsx
@@ -85,29 +85,72 @@ export const actions = (store) => {
     const ctx     = cnv.getContext("2d");
     createImageBitmap(bigCanvas).then(function(imgBitmap) {
         ctx.drawImage(imgBitmap, 0, 0);
-        const imgData = ctx.getImageData(0, 0, cnv.width, cnv.height);
-        // for (let i = 0; i < imgData.data.length; i += 4) {
-        //     imgData.data[i] = 255 - imgData.data[i];
-        //     imgData.data[i+1] = 255 - imgData.data[i+1];
-        //     imgData.data[i+2] = 255 - imgData.data[i+2];
-        //     imgData.data[i+3] = 255;
-        // }
+        const imgData   = ctx.getImageData(0, 0, cnv.width, cnv.height);
         const pixels    = imgData.data;
         const numPixels = imgData.width * imgData.height;
-        for (var i = 0; i < numPixels; i++) {
-            pixels[i*4] = 255-pixels[i*4]; // Red
-            pixels[i*4+1] = 255-pixels[i*4+1]; // Green
-            pixels[i*4+2] = 255-pixels[i*4+2]; // Blue
+        for (let i = 0; i < numPixels; i++) {
+            pixels[i*4]   = 255-pixels[i*4];    // Red Channel
+            pixels[i*4+1] = 255-pixels[i*4+1];  // Green Channel
+            pixels[i*4+2] = 255-pixels[i*4+2];  // Blue Channel
         };
         ctx.putImageData(imgData, 0, 0, 0, 0, 800, 600);
+        store.setState({ bigCanvas: imgData });
+    });
+
+  }
+
+  const updateCanvasStripRed = ({ bigCanvas }) => {
+
+    const cnv     = trsdoc.qs('#big-canvas-window');
+    const ctx     = cnv.getContext("2d");
+    createImageBitmap(bigCanvas).then(function(imgBitmap) {
+        ctx.drawImage(imgBitmap, 0, 0);
+        const imgData   = ctx.getImageData(0, 0, cnv.width, cnv.height);
+        const pixels    = imgData.data;
+        const numPixels = imgData.width * imgData.height;
+        for (let i = 0; i < numPixels; i++) {
+            pixels[i*4]   = 0;    // Red Channel
+        };
+        ctx.putImageData(imgData, 0, 0, 0, 0, 800, 600);
+        store.setState({ bigCanvas: imgData });
     });
 
   }
 
   const updateCanvasStripBlue = ({ bigCanvas }) => {
-    // const workerPkg = [someVar];
-    // uniops.bindOperator.replace(operatorB, store, 'bigCanvas');
-    // canvasOpB.postMessage(workerPkg);
+
+    const cnv     = trsdoc.qs('#big-canvas-window');
+    const ctx     = cnv.getContext("2d");
+    createImageBitmap(bigCanvas).then(function(imgBitmap) {
+        ctx.drawImage(imgBitmap, 0, 0);
+        const imgData   = ctx.getImageData(0, 0, cnv.width, cnv.height);
+        const pixels    = imgData.data;
+        const numPixels = imgData.width * imgData.height;
+        for (let i = 0; i < numPixels; i++) {
+            pixels[i*4+1] = 0;  // Blue Channel
+        };
+        ctx.putImageData(imgData, 0, 0, 0, 0, 800, 600);
+        store.setState({ bigCanvas: imgData });
+    });
+
+  }
+
+  const updateCanvasStripGreen = ({ bigCanvas }) => {
+
+    const cnv     = trsdoc.qs('#big-canvas-window');
+    const ctx     = cnv.getContext("2d");
+    createImageBitmap(bigCanvas).then(function(imgBitmap) {
+        ctx.drawImage(imgBitmap, 0, 0);
+        const imgData   = ctx.getImageData(0, 0, cnv.width, cnv.height);
+        const pixels    = imgData.data;
+        const numPixels = imgData.width * imgData.height;
+        for (let i = 0; i < numPixels; i++) {
+            pixels[i*4+2] = 0;  // Green Channel
+        };
+        ctx.putImageData(imgData, 0, 0, 0, 0, 800, 600);
+        store.setState({ bigCanvas: imgData });
+    });
+
   }
 
   const updateCanvasC = ({ bigCanvas }) => {
@@ -119,7 +162,9 @@ export const actions = (store) => {
   return {
     loadCanvas,
     updateCanvasInvert,
+    updateCanvasStripRed,
     updateCanvasStripBlue,
+    updateCanvasStripGreen,
     updateCanvasC
   }
 }
@@ -134,14 +179,18 @@ export const BigCanvas = connect(['bigCanvas'], actions)(
     bigCanvas,
     loadCanvas,
     updateCanvasInvert,
+    updateCanvasStripRed,
     updateCanvasStripBlue,
+    updateCanvasStripGreen,
     updateCanvasC
   }) => (
       <ul class="wrapper">
         <li>
           <span>Offload Canvas/Pixel manipulation to a background thread | </span>
           <button onClick={e => updateCanvasInvert(e)} type="button" name="button">Invert the Image</button>
-          <button onClick={e => updateCanvasStripBlue(e)} type="button" name="button">Strip the Blue Channel</button>
+          <button onClick={e => updateCanvasStripRed(e)} type="button" name="button">Strip Red Channel</button>
+          <button onClick={e => updateCanvasStripGreen(e)} type="button" name="button">Strip Green Channel</button>
+          <button onClick={e => updateCanvasStripBlue(e)} type="button" name="button">Strip Blue Channel</button>
           <button onClick={e => updateCanvasB(e)} type="button" name="button">Run updateCanvasB</button>
           <button onClick={e => updateCanvasC(e)} type="button" name="button">Run updateCanvasC</button>
           <ul>
