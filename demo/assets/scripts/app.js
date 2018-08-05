83b2277ae50c1bbae396dafcb0513a6341307f84
Alexander Reid <alexander.a.reid@espn.com>
2018-08-05 00:19:48 -0400

    Canvas examples 9

--- demo/assets/scripts/app.js
+++ demo/assets/scripts/app.js
@@ -2,21 +2,31 @@ import { h, render }  from 'preact';
 import 'babel-polyfill'
 import '../styles/app.scss'
 
-import BigArray         from './components/BigArray';
-import BigObject        from './components/BigObject';
-import BigObjectSymbols from './view/bigObjectSymbols';
-import BigString        from './components/BigString';
-import BigCanvas        from './components/BigCanvas';
+import BigObjectEq  from './view/bigObjectEq';
+import BigArrayEq   from './view/bigArrayEq';
+import BigCanvasEq  from './view/bigCanvasEq';
+import BigStringEq  from './view/bigStringEq';
+import BigArray     from './components/BigArray';
+import BigObject    from './components/BigObject';
+import BigString    from './components/BigString';
+import BigCanvas    from './components/BigCanvas';
 
-const trsdoc            = require('trsdoc')(document);
-const bigObjectSymbols  = trsdoc.ge_byid('bigObjectSymbols');
-const bigObject         = trsdoc.ge_byid('bigObject');
-const bigArray          = trsdoc.ge_byid('bigArray');
-const bigCanvas         = trsdoc.ge_byid('bigCanvas');
-const bigString         = trsdoc.ge_byid('bigString');
+const trsdoc        = require('trsdoc')(document);
+const bigObjectEq   = trsdoc.ge_byid('bigObjectEq');
+const bigArrayEq    = trsdoc.ge_byid('bigArrayEq');
+const bigCanvasEq   = trsdoc.ge_byid('bigCanvasEq');
+const bigStringEq    = trsdoc.ge_byid('bigStringEq');
+const bigObject     = trsdoc.ge_byid('bigObject');
+const bigArray      = trsdoc.ge_byid('bigArray');
+const bigCanvas     = trsdoc.ge_byid('bigCanvas');
+const bigString     = trsdoc.ge_byid('bigString');
+
+render(<BigObjectEq />, bigObjectEq, bigObjectEq.lastChild);
+// render(<BigArrayEq />, bigArrayEq, bigArrayEq.lastChild);
+// render(<BigCanvasEq />, bigCanvasEq, bigCanvasEq.lastChild);
+// render(<BigStringEq />, bigStringEq, bigStringEq.lastChild);
 
 render(<BigObject />, bigObject, bigObject.lastChild);
-render(<BigObjectSymbols />, bigObjectSymbols, bigObjectSymbols.lastChild);
 render(<BigArray />, bigArray, bigArray.lastChild);
 render(<BigCanvas />, bigCanvas, bigCanvas.lastChild);
 render(<BigString />, bigString, bigString.lastChild);
---
+++ demo/assets/scripts/view/bigStringEq.jsx
@@ -0,0 +1,16 @@
+import { h, Component } from 'preact';
+
+export default class bigObjectSymbols extends Component {
+  render() {
+    return (
+      <mark>
+        <i class="em em-face_with_symbols_on_mouth"></i> +
+        <i class="em em-dart"></i>
+        <i class="em em-clap"></i>
+        <i class="em em-100"></i>
+        <i class="em em-boom"></i> =
+        <i class="em em-relieved"></i>
+      </mark>
+    )
+  }
+}
